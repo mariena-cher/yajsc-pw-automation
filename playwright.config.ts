fead3726bdef2864d0ca039b6c3268d2374f95a3
mariena-cher <chernoholova@gmail.com>
2025-09-08 21:32:38 +0300

    Refactor reporter configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'always' }], ['dot'], ['json', { outputFile: 'results.json' }]],
+  reporter: [
+    ['html', { open: 'on-failure' }],
+    ['dot'],
+    ['json', { outputFile: 'results.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
